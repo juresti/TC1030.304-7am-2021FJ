7c9ef0d4b022783a0cdad306c5ddbb3c878587c2
Jorge Adolfo Ramírez Uresti <juresti@tec.mx>
2021-05-10 09:10:20 -0500

    Ejemplo uso clases

---
+++ Clase1/Cliente.cpp
@@ -0,0 +1,7 @@
+//NOmbre matricula
+
+#include "Cliente.h"
+
+Cliente::Cliente() : edad{18} {}
+int Cliente::getEdad() {return edad;}
+void Cliente::setEdad(int e) {edad = e;}
---
+++ Clase1/Cliente.h
@@ -0,0 +1,13 @@
+#ifndef CLIENTE_H
+#define CLEINTE_H
+
+class Cliente{
+private:
+    int edad;
+public:
+    Cliente();
+    int getEdad();
+    void setEdad(int e);
+}; //Cliente
+
+#endif
