0a77371a7809841642d664793176aeae8ebaee36
Jorge Adolfo Ramírez Uresti <juresti@tec.mx>
2021-05-24 08:53:53 -0500

    Parte final del ejercicio de Polimorfismo

--- EjemploPolimorfismo/ejPolimorfismo.cpp
+++ EjemploPolimorfismo/ejPolimorfismo.cpp
@@ -3,6 +3,7 @@
 #include "Electrodomestico.h"
 #include "Microondas.h"
 #include "Licuadora.h"
+#include "Freidora.h"
 #include <iostream>
 using namespace std;
 
@@ -15,11 +16,14 @@ int main(int argc, char const *argv[])
 {
     Microondas micro1{7,"Rosa"};
     Licuadora licua1{13};
+    Freidora frei1{"Rostizando"};
 
     cout << micro1.toString() << endl;
     cout << licua1.toString() << endl;
+    cout << frei1.toString() << endl;
 
     cout << reporteBonitoElectro(micro1) << endl;
     cout << reporteBonitoElectro(licua1) << endl;
+    cout << reporteBonitoElectro(frei1) << endl;
     return 0;
 }
